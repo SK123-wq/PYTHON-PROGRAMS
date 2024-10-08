23a42dfb947fcd0dc61a19a9985d4540db137204
SK123-wq <shivanshkhetan2012@gmail.com>
2024-10-08 18:01:07 +0545

    dsdd

---
+++ Pandadataframe.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+examdata={"name":["aarav","yaksh","ram","sita","jhon","jerry","emily","bose","geoffry","lakshman"],
+          "score":[np.nan,30,90,50,40,50,np.nan,49,50,40],"attempts":[1,2,3,1,4,2,1,1,1,2],"qualify":["yes","no","no","yes","yes","yes","yes","no","yes","yes"]}
+labels=["a","b","c","d","e","f","g","h","i","j"]
+df=pd.DataFrame(examdata,index=labels)
+print("Summary of basic information about this data frame and its data")
+print(df.info())
+print(df.head())
+print(df.tail())
